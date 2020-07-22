6b7b00757e3c479eeb6186163da1cbc2ed98e227
ANDREA XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEAN2@OOCL.COM>
2020-07-22 15:24:25 +0800

    practice01 andrea feature: add name and age in Person

--- src/main/java/practice02/Student.java
+++ src/main/java/practice02/Student.java
@@ -1,4 +1,26 @@
 package practice02;
 
-public class Student {
+public class Student extends Person{
+    protected int classNum;
+//    public Student(String name, int age) {
+//        super(name, age);
+//    }
+
+
+    public int getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(int classNum) {
+        this.classNum = classNum;
+    }
+
+    public Student(String name, int age, int classNum) {
+        super(name, age);
+        this.classNum = classNum;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class " + this.classNum;
+    }
 }
