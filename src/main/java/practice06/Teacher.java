9326c6fcfcbe1b3f5dbc677c5af1a40db3464e3e
XIEAN2 <andreatwelve@sina.cn>
2020-07-22 18:40:10 +0800

    practice06 andrea feature: add variable of klass, construction,introduce in Teacher

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Integer getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Integer klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        String str = super.introduce().concat(" I am a Teacher. I teach ");
+        if (this.klass != null) {
+            return  str.concat(String.format("Class %s.", this.klass));
+        } else {
+            return str.concat("No Class.");
+        }
+    }
 }
