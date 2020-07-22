a788a488d0770e4b24585e1aad514553700bb63e
andrea <andreatwelve@sina.cn>
2020-07-22 23:35:06 +0800

    Practice09Test andrea feature: add in Teacher

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,40 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (this.klass == null) {
+            return super.introduce().concat(" I am a Teacher. I teach No Class.");
+        } else {
+            return super.introduce().concat(String.format(" I am a Teacher. I teach Class %s.", this.klass.getNumber()));
+        }
+    }
+
+    public String introduceWith(Student student){
+        if (this.klass != null && student.getKlass().getNumber() == this.getKlass().getNumber()) {
+            return super.introduce().concat(String.format(" I am a Teacher. I teach %s.", student.getName()));
+
+        } else {
+            return super.introduce().concat(String.format(" I am a Teacher. I don't teach %s.", student.getName()));
+        }
+    }
 }
