98acb03d60abe43ff20a03a0118528e00da8b5e2
XIEAN2 <andreatwelve@sina.cn>
2020-07-22 19:24:52 +0800

    practice08 andrea feature: add Person, Klass, Student, Teacher

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,34 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s", this.number);
+    }
+
+    public void assignLeader(Student student) {
+        this.leader = student;
+    }
 }
