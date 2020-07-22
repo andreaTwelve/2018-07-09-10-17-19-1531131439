a88eb5e1ec560ce27641daa7ced362eaa14bee67
andrea <andreatwelve@sina.cn>
2020-07-22 23:34:09 +0800

    Practice09Test andrea feature: add in Student

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,29 @@
 package practice09;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (this.klass != null && this.klass.getLeader() != null && this.klass.getLeader().getId() == this.getId()) {
+            return super.introduce().concat(String.format(" I am a Student. I am Leader of Class %s.", this.klass.getNumber()));
+        } else {
+            return super.introduce().concat(String.format(" I am a Student. I am at Class %s.", this.klass.getNumber()));
+        }
+    }
+
+
 }
