1d0f6104f50f6d84da89209fe92c804df04ecee5
XIEAN2 <andreatwelve@sina.cn>
2020-07-22 19:22:57 +0800

    practice07 andrea feature: add Person, Teacher, Klass, Student

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,25 @@
 package practice07;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Student(String name, int age , Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+
+
+    @Override
+    public String introduce() {
+        return super.introduce().concat(String.format(" I am a Student. I am at Class %s.", this.klass.getNumber()));
+    }
 }
--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,37 @@
 package practice07;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        if (this.klass != null) {
+            return super.introduce().concat(String.format(" I am a Teacher. I teach Class %s.", this.klass.getNumber()));
+        } else {
+            return super.introduce().concat(String.format(" I am a Teacher. I teach No Class."));
+        }
+    }
+
+    public String introduceWith(Student student) {
+        if (student.getKlass().getNumber() == this.klass.getNumber()) {
+            return super.introduce().concat(String.format(" I am a Teacher. I teach %s.", student.getName()));
+        } else {
+            return super.introduce().concat(String.format(" I am a Teacher. I don't teach %s.", student.getName()));
+        }
+    }
 }
