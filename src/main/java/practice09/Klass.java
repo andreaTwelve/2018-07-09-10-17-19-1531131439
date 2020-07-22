63b621b29e9cfce2d8852b348c292885b9f9bfc0
andrea <andreatwelve@sina.cn>
2020-07-22 23:23:25 +0800

    Practice09Test andrea feature: add in Klass

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,56 @@
 package practice09;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private List<Student> studentList = new ArrayList<>();
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public void setLeader(Student leader) {
+        this.leader = leader;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public void assignLeader(Student leader) {
+        if (studentList.size() > 0) {
+            for (Student stu: this.studentList) {
+                if (stu.getId() != leader.getId()) {
+                    System.out.print("It is not one of us.\n");
+                } else {
+                    this.leader = leader;
+                }
+            }
+        } else {
+            System.out.print("It is not one of us.\n");
+        }
+    }
+
+    public void appendMember(Student student) {
+        if (student != null) {
+            this.studentList.add(student);
+        }
+
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %s", this.number);
+    }
+
 }
